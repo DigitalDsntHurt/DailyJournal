307c8bd658863687b5fd2baa669722159dc37827
N Smith <digitaldoesnthurt@gmail.com>
2017-03-23 15:29:12 -0700

    final(?) fix for dating problems on new entry_date field of Entries model

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -70,6 +70,6 @@ class EntriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def entry_params
-      params.require(:entry).permit(:grateful_1, :grateful_2, :grateful_3, :make_today_great_1, :make_today_great_2, :make_today_great_3, :affirmation_1, :affirmation_2, :amazing_thing_1, :amazing_thing_2, :amazing_thing_3, :improve_today_1, :improve_today_2, :anything_else)
+      params.require(:entry).permit(:grateful_1, :grateful_2, :grateful_3, :make_today_great_1, :make_today_great_2, :make_today_great_3, :affirmation_1, :affirmation_2, :amazing_thing_1, :amazing_thing_2, :amazing_thing_3, :improve_today_1, :improve_today_2, :anything_else, :entry_date)
     end
 end
