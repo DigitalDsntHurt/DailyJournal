0bed3d676a81b1db560edfd6ccff2f50fb5d54b6
N Smith <digitaldoesnthurt@gmail.com>
2017-03-23 13:54:38 -0700

    migration adds an entry_date column to Entries model + forces a uniqueness constraint at the model level

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,10 @@
 class Entry < ApplicationRecord
+	before_validation :set_entry_date
+	validates_uniqueness_of :entry_date
+	private
+	def set_entry_date
+		if self.entry_date == nil
+			self.entry_date = self.created_at
+		end
+	end
 end
