358d1f64052973204616b882bfec59cc1ccbe412
N Smith <digitaldoesnthurt@gmail.com>
2017-03-23 15:17:54 -0700

    date cleanup / heroku debugging

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,11 +1,13 @@
 class Entry < ApplicationRecord
-	before_validation :set_entry_date
+	#before_validation :set_entry_date
+	validates_presence_of :entry_date
 	validates_uniqueness_of :entry_date
+	
 	private
-	def set_entry_date
-		if self.entry_date == nil
+	#def set_entry_date
+		#if self.entry_date == nil
 			#self.entry_date = self.created_at
-			self.update(entry_date: Date.today)
-		end
+			#self.update(entry_date: Date.today)
+		#end
 	end
 end
