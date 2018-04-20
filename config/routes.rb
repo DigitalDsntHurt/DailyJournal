9d0e8f068dd7cf969cf7ab5c7380a5a97c8b13f7
N Smith <digitaldoesnthurt@gmail.com>
2018-04-19 22:48:46 -0700

    daily journal counts to journal history view

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,18 @@
 Rails.application.routes.draw do
 
+  get 'home/index'
+  root 'dashboards#daily_dash_2018'
+
+  resources :climbs
   resources :workouts
   resources :everydays
   resources :weights
   resources :simple_consumables
 
-  get 'exercises/copy' => 'exercises#copy'
-  get 'exercises/stretch' => 'exercises#stretch'
-  get 'exercises/core' => 'exercises#core'
-  get 'exercises/run' => 'exercises#run'
-  get 'exercises/kettlebell' => 'exercises#kettlebell'
-  resources :exercises
-
-  resources :priorities
-
   get 'morning_rituals/journal_dash_form' => 'morning_rituals#journal_dash_form'
   resources :morning_rituals
   resources :evening_rituals
-  
-  
+
   get 'dashboards/daily_dash_2018'
   get 'dashboards/yesterday_dash_2018'
   get 'dashboards/weekly_dash_2018'
@@ -40,7 +34,6 @@ Rails.application.routes.draw do
   get 'consumables/omelette' => 'consumables#omelette'
   get 'consumables/mexican' => 'consumables#mexican'
   get 'consumables/food_binge' => 'consumables#food_binge'
-
   get 'consumables/drink' => 'consumables#drink'
   get 'consumables/drink_water' => 'consumables#drink_water'
   get 'consumables/drink_wine' => 'consumables#drink_wine'
@@ -48,14 +41,17 @@ Rails.application.routes.draw do
   get 'consumables/drink_binge' => 'consumables#drink_binge'
   post 'consumables/drink_water_now' => 'consumables#drink_water_now'
   post 'consumables/drink_coffee_now' => 'consumables#drink_coffee_now'
-
   post 'consumables/new_spliff_now' => 'consumables#new_spliff_now'
   get 'consumables/spliff_binge' => 'consumables#spliff_binge'
   resources :consumables
 
-  resources :climbs
+  get 'exercises/copy' => 'exercises#copy'
+  get 'exercises/stretch' => 'exercises#stretch'
+  get 'exercises/core' => 'exercises#core'
+  get 'exercises/run' => 'exercises#run'
+  get 'exercises/kettlebell' => 'exercises#kettlebell'
+  resources :exercises
 
-  get 'home/index'
-  root 'dashboards#daily_dash_2018'
+  resources :priorities
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
